deb6ad3fb60a151f9e03ee80cee8cd2c1853c58d
adoydevs <imranadoy@gmail.com>
2023-12-05 12:42:13 +0600

    add

--- skilled-e/app/page.tsx
+++ skilled-e/app/page.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './components/About'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
-      
+      <About />
+      <Footer />
     </main>
   )
 }
